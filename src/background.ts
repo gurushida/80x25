5ccfe3b8cec8d85702df08a899d3cfb57728fe12
gurushida <49831684+gurushida@users.noreply.github.com>
2020-06-07 19:06:49 +0200

    Added ice cream shop background

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,4 @@
 import { loadBackground } from "./images";
 
 export const bank = loadBackground('asciiart/backgrounds/bank.txt');
+export const ice_cream_shop = loadBackground('asciiart/backgrounds/ice_cream_shop.txt');
--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,10 @@ ui.addClickListener(e => ui.screen.title = `click at ${e.X}x${e.Y}`);
 ui.addMoveListener(e => ui.screen.title = `move at ${e.X}x${e.Y}`);
 
 ui.buffer.paint(background.bank, 0, 0);
-
 ui.buffer.paint(sprites.boom_blaster1, 30, 18);
 
+ui.buffer.clear();
+
+ui.buffer.paint(background.ice_cream_shop, 0, 0);
 ui.render();
 
