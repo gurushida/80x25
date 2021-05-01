cf08464dc67f7bfc8696cb1f1880119dd478afee
gurushida <49831684+gurushida@users.noreply.github.com>
2021-05-01 06:06:40 +0200

    Refactored bird animations

--- src/resources/animations/bird.ts
+++ src/resources/animations/bird.ts
@@ -4,36 +4,11 @@ import { SPR_BIRD_0 } from "../generated/images/SPR_BIRD_0";
 import { SPR_BIRD_1 } from "../generated/images/SPR_BIRD_1";
 import { SPR_BIRD_2 } from "../generated/images/SPR_BIRD_2";
 
-export const ANIM_BIRD = new ImageAnimation(3, 1, ZIndex.BEHIND_GUY, true, undefined,
-    left => left % 85,
-    [
-        {
-            image: SPR_BIRD_0,
-            durationInTicks: 5,
-            offsetX: 0,
-            offsetY: 0,
-        },
-        {
-            image: SPR_BIRD_1,
-            durationInTicks: 5,
-            offsetX: 0,
-            offsetY: 0,
-        },
-        {
-            image: SPR_BIRD_2,
-            durationInTicks: 5,
-            offsetX: 1,
-            offsetY: 0,
-        },
-        {
-            image: SPR_BIRD_1,
-            durationInTicks: 5,
-            offsetX: 0,
-            offsetY: 0,
-        },
-    ]);
+export const ANIM_BIRD = createBirdAnimation(3, 1);
+export const ANIM_BIRD2 = createBirdAnimation(0, 2);
 
-    export const ANIM_BIRD2 = new ImageAnimation(0, 2, ZIndex.BEHIND_GUY, true, undefined,
+function createBirdAnimation(initialLeft: number, initialTop: number) {
+    return new ImageAnimation(initialLeft, initialTop, ZIndex.BEHIND_GUY, true, undefined,
         left => left % 85,
         [
             {
@@ -61,4 +36,4 @@ export const ANIM_BIRD = new ImageAnimation(3, 1, ZIndex.BEHIND_GUY, true, undef
                 offsetY: 0,
             },
         ]);
-    
+}
