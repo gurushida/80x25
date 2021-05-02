043ea3c7d359dc78446e607b9d016029912962ef
gurushida <49831684+gurushida@users.noreply.github.com>
2021-05-02 07:23:45 +0200

    Made arcade background

--- src/resources/compileResources.ts
+++ src/resources/compileResources.ts
@@ -13,7 +13,7 @@ fs.mkdirSync(DIALOGS_DIR, { recursive: true });
 compileOpaqueImage('asciiart/backgrounds/bank.txt', 'BG_BANK');
 compileOpaqueImage('asciiart/backgrounds/ice_cream_shop.txt', 'BG_ICE_CREAM_SHOP');
 compileOpaqueImage('asciiart/backgrounds/pub.txt', 'BG_PUB');
-compileOpaqueImage('asciiart/sprites/arcade_counter.txt', 'SPR_ARCADE_COUNTER');
+compileOpaqueImage('asciiart/backgrounds/arcade.txt', 'BG_ARCADE');
 
 
 //--------------------------------------------------------------------------
--- src/resources/scenes/arcade.ts
+++ src/resources/scenes/arcade.ts
@@ -1,24 +1,26 @@
-import { createMaskHotspot, GuyPosition, HotspotId } from "@/hotspots";
+import { createMaskHotspot, createRectangleHotspot, GuyPosition, HotspotId } from "@/hotspots";
 import { PaintTask, getPaintTask } from "@/paintTask";
 import { SceneLoader, SceneId, SceneData } from "@/scene";
 import { Triggers } from "@/triggers";
 import { ZIndex } from "@/zIndex";
 import { ANIM_ARCADE_SIGN } from "../animations/arcade_sign";
 import { ANIM_LAMP } from "../animations/lamp";
+import { BG_ARCADE } from "../generated/images/BG_ARCADE";
 import { SPR_ARCADE_CABINET } from "../generated/images/SPR_ARCADE_CABINET";
 import { SPR_ARCADE_COUNTER } from "../generated/images/SPR_ARCADE_COUNTER";
 import { SPR_ARCADE_TABLE } from "../generated/images/SPR_ARCADE_TABLE";
 
-const game1: PaintTask = getPaintTask(SPR_ARCADE_CABINET, 0, 10, ZIndex.BEHIND_GUY, createMaskHotspot(SPR_ARCADE_CABINET, HotspotId.ARCADE_GAME1));
-const game2: PaintTask = getPaintTask(SPR_ARCADE_CABINET, 11, 10, ZIndex.BEHIND_GUY, createMaskHotspot(SPR_ARCADE_CABINET, HotspotId.ARCADE_GAME2));
-const game3: PaintTask = getPaintTask(SPR_ARCADE_CABINET, 22, 10, ZIndex.BEHIND_GUY, createMaskHotspot(SPR_ARCADE_CABINET, HotspotId.ARCADE_GAME3));
+const game1: PaintTask = getPaintTask(SPR_ARCADE_CABINET, 0, 11, ZIndex.BEHIND_GUY, createMaskHotspot(SPR_ARCADE_CABINET, HotspotId.ARCADE_GAME1));
+const game2: PaintTask = getPaintTask(SPR_ARCADE_CABINET, 11, 11, ZIndex.BEHIND_GUY, createMaskHotspot(SPR_ARCADE_CABINET, HotspotId.ARCADE_GAME2));
+const game3: PaintTask = getPaintTask(SPR_ARCADE_CABINET, 22, 11, ZIndex.BEHIND_GUY, createMaskHotspot(SPR_ARCADE_CABINET, HotspotId.ARCADE_GAME3));
 
-const game4: PaintTask = getPaintTask(SPR_ARCADE_CABINET, 48, 10, ZIndex.BEHIND_GUY, createMaskHotspot(SPR_ARCADE_CABINET, HotspotId.ARCADE_GAME4));
-const game5: PaintTask = getPaintTask(SPR_ARCADE_CABINET, 59, 10, ZIndex.BEHIND_GUY, createMaskHotspot(SPR_ARCADE_CABINET, HotspotId.ARCADE_GAME5));
-const game6: PaintTask = getPaintTask(SPR_ARCADE_CABINET, 70, 10, ZIndex.BEHIND_GUY, createMaskHotspot(SPR_ARCADE_CABINET, HotspotId.ARCADE_GAME6));
+const game4: PaintTask = getPaintTask(SPR_ARCADE_CABINET, 48, 11, ZIndex.BEHIND_GUY, createMaskHotspot(SPR_ARCADE_CABINET, HotspotId.ARCADE_GAME4));
+const game5: PaintTask = getPaintTask(SPR_ARCADE_CABINET, 59, 11, ZIndex.BEHIND_GUY, createMaskHotspot(SPR_ARCADE_CABINET, HotspotId.ARCADE_GAME5));
+const game6: PaintTask = getPaintTask(SPR_ARCADE_CABINET, 70, 11, ZIndex.BEHIND_GUY, createMaskHotspot(SPR_ARCADE_CABINET, HotspotId.ARCADE_GAME6));
 
 const table: PaintTask = getPaintTask(SPR_ARCADE_TABLE, 62, 21, ZIndex.FRONT, undefined);
-const counter: PaintTask = getPaintTask(SPR_ARCADE_COUNTER, 32, 7, ZIndex.BEHIND_GUY, undefined);
+
+const arcadeBackground: PaintTask = getPaintTask(BG_ARCADE, 0, 0, ZIndex.BACKGROUND, undefined);
 
 
 const initialGuyPosition: GuyPosition = {
@@ -34,7 +36,7 @@ export const ARCADE_LOADER: SceneLoader = {
         return {
             showActionBar: true,
             guyPosition: initialGuyPosition,
-            images: [game1, game2, game3, game4, game5, game6, table, counter],
+            images: [arcadeBackground, game1, game2, game3, game4, game5, game6, table,],
             animations: [ANIM_LAMP, ANIM_ARCADE_SIGN],
             hotspots: [],
         };
