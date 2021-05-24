4811e6d15655d12075629cca6a87b82bc4d64720
gurushida <49831684+gurushida@users.noreply.github.com>
2021-05-24 10:11:27 +0200

    Added text

--- src/resources/scenes/jewellery_store_outside.ts
+++ src/resources/scenes/jewellery_store_outside.ts
@@ -12,12 +12,18 @@ import { SPR_FIRE_HYDRANT_0 } from "../generated/images/SPR_FIRE_HYDRANT_0";
 
 const background: PaintTask = getPaintTask(BG_JEWELLERY_STORE_OUTSIDE, 0, 0, ZIndex.BEHIND_GUY, createFullHotspot(HotspotId.JEWELLERY_STORE));
 
-const ring_sign = getPaintTask(SPR_JEWELLERY_RING_SIGN, 33, 4, ZIndex.BEHIND_GUY, undefined);
-const sign = getPaintTask(SPR_JEWELLERY_SIGN, 56, 6, ZIndex.BEHIND_GUY, createMaskHotspot(SPR_JEWELLERY_SIGN, HotspotId.JEWELLERY_SIGN));
+const ring_sign = getPaintTask(SPR_JEWELLERY_RING_SIGN, 26, 4, ZIndex.BEHIND_GUY, undefined);
+const sign = getPaintTask(SPR_JEWELLERY_SIGN, 56, 5, ZIndex.BEHIND_GUY, createMaskHotspot(SPR_JEWELLERY_SIGN, HotspotId.JEWELLERY_SIGN));
 const suv = getPaintTask(SPR_SUV_0, 20, 12, ZIndex.FRONT, createMaskHotspot(SPR_SUV_0, HotspotId.SUV));
 const fire_hydrant = getPaintTask(SPR_FIRE_HYDRANT_0, 15, 15, ZIndex.BEHIND_GUY, createMaskHotspot(SPR_FIRE_HYDRANT_0, HotspotId.FIRE_HYDRANT));
 
 const hotspots: Hotspot[] = [
+    {
+        hotspotId: HotspotId.JEWELLERY_STORE,
+        description: 'jewellery store',
+        rightClickAction: ActionBarButton.LOOK,
+        lookAt: [['A place where you can find', 'plain normal jewellery,'], ['without any magical power', 'from some Norse dwarf'], ['or some curse from', 'a dude in a volcano.']]
+    },
     {
         hotspotId: HotspotId.JEWELLERY_SIGN,
         description: 'sign',
@@ -28,13 +34,16 @@ const hotspots: Hotspot[] = [
         hotspotId: HotspotId.SUV,
         description: 'big car',
         rightClickAction: ActionBarButton.LOOK,
-        lookAt: [['I don\'t know what is worse with this vehicle,', 'clearly not designed for cities:'], ['the embarrassing display of how much', 'the owner has to compensate for,'], ['or the fact that it manages to be', 'parked illegally in three different ways,'], ['blocking the sidewalk, a fire hydrant', 'and the entrance to this shop.']]
+        lookAt: [['I don\'t know what is worse with this vehicle,', 'clearly not designed for cities:'], ['the embarrassing display of how much', 'the owner has to compensate for,'], ['or the fact that it manages to be parked', 'illegally in three different ways,'], ['blocking the sidewalk, a fire hydrant', 'and the entrance to this shop.']]
     },
     {
         hotspotId: HotspotId.FIRE_HYDRANT,
         description: 'fire hydrant',
         rightClickAction: ActionBarButton.LOOK,
-        lookAt: [['Let\'s hope there is no fire today,'], ['because access to this fire hydrant', 'is blocked by this big car.']]
+        lookAt: [['Let\'s hope there is no fire today,'], ['because access to this fire hydrant', 'is blocked by this big car.']],
+        useDirectly: {
+            comment: [['I\'m not saying I would be totally against', 'messing up with some public property,'], ['but without proper tooling'], ['it would take a considerable amount', 'of force to blow this thing open.']],
+        }
     },
 ];
 
